dd3c23404809a4b61bc3c66739e59819316a8ab1
Mike Brainerd <mbrainerd@d2.com>
2017-08-09 14:39:10 -0700

    fixed typo and cleanup

--- hooks/pick_environment.py
+++ hooks/pick_environment.py
@@ -13,7 +13,6 @@ Hook which chooses an environment file to use based on the current context.
 """
 
 from tank import Hook
-from tank import TemplatePath
 
 class PickEnvironment(Hook):
 
@@ -24,9 +23,6 @@ class PickEnvironment(Hook):
 
         DD implementation includes Sequence and Step
         """
-
-
-
         if context.project is None:
             # our context is completely empty!
             # don't know how to handle this case.
@@ -44,19 +40,16 @@ class PickEnvironment(Hook):
                 return "sequence"
             if context.entity["type"] == "Shot":
                 return "shot"
-            if context.entity["type"] == "Step":
-                return "shot"
             if context.entity["type"] == "Asset":
                 return "asset"
+
         if context.entity and context.step:
             # we have a step and an entity
             if context.entity["type"] == "Project":
-                return "project_stp"
+                return "project_step"
             if context.entity["type"] == "Sequence":
                 return "sequence_step"
             if context.entity["type"] == "Shot":
-                return "shot_step"
-            if context.entity["type"] == "Step":
                 return "shot_step"
             if context.entity["type"] == "Asset":
                 return "asset_step"
