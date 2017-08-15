7b5a76a5489fc0d96974f9b9692c7796443f46ec
Mike Brainerd <mbrainerd@d2.com>
2017-08-15 15:02:03 -0700

    updated to support defining one pc config for all shows and distinguishing using DD_SHOW instead of needing to have a pc config per show

--- python/tank/pipelineconfig_utils.py
+++ python/tank/pipelineconfig_utils.py
@@ -110,19 +110,34 @@ def get_pipeline_metadata(pipeline_config_path):
                         "support! File: '%s' Error: %s" % (cfg_yml, e))
 
     # DD Hackery: Get the project from DD_SHOW instead of from yaml file
-    if not data.get("project_id", None):
+    if not data.get("pc_id", None):
         dd_show = os.environ.get("DD_SHOW", None)
         if dd_show:
             # Get the matching project entity
             proj_entity = shotgun.get_entity(dd_show, "Project")
             if proj_entity:
-                # Filter PipelineConfigurations by this project
-                pc_name = data.get("pc_name")
+
+                # Default is "Primary"
+                pc_name = data.get("pc_name", "Primary")
+
+                # Get the PipelineConfiguration, filtered by this project
                 pc_entity = shotgun.get_entity(pc_name, "PipelineConfiguration", [["project", "is", proj_entity]])
                 if pc_entity:
-                    data["project_name"]    = dd_show
+                    data["project_name"]    = proj_entity.get("name")
                     data["project_id"]      = proj_entity.get("id")
                     data["pc_id"]           = pc_entity.get("id")
+                    data["pc_name"]         = pc_entity.get("code")
+
+        # Else return the Site PipelineConfiguration
+        else:
+            # Default is "Primary"
+            pc_name = data.get("pc_name", "Primary")
+
+            # Get the PipelineConfiguration, filtered by this project
+            pc_entity = shotgun.get_entity(pc_name, "PipelineConfiguration", [["id", "is", 1]])
+            if pc_entity:
+                data["pc_id"]           = pc_entity.get("id")
+                data["pc_name"]         = pc_entity.get("code")
 
     return data
 
