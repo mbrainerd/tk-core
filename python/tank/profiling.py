d488528d03b3e1a668ce1cb668b0d38ffdcf558b
Garvit Verma <gverma@d2.com>
2018-05-29 16:03:10 +0530

    profiler now supports one instance of profiler per module

--- python/tank/profiling.py
+++ python/tank/profiling.py
@@ -2,10 +2,15 @@
 import pstats
 import cProfile
 
-
 class CProfileMethodRunner(object):
     """
-    How to use:
+    Notes::
+
+    Creates a new profiler object for each and every different module the method is run on... possible conflicts.
+    If the stop_profiler classmethod is never called since SGTK import modules with custom names, and it might happen
+    that the stop_profiler doesn't accept the profiler to stop since the module name is not the same anymore.
+
+    Usage::
 
     from sgtk import CProfileMethodRunner
 
@@ -19,7 +24,7 @@ class CProfileMethodRunner(object):
         exit_point_function_contents
     """
 
-    profiler = None
+    profiler_mapping = dict()
 
     def __init__(self, profiling_output, init_new_profiler, stop_profiler, save_stats):
         """
@@ -59,40 +64,43 @@ class CProfileMethodRunner(object):
         Call function runs the wrapped function to perform the decoration.
         """
         def wrapped_f(*args, **kwargs):
+            func_module = func.__module__
+            func_name = func.__name__
             try:
                 # init the class profiler
-                if not CProfileMethodRunner.profiler and self.init_new_profiler:
-                    CProfileMethodRunner.profiler = cProfile.Profile()
-                    print "Starting CProfiler on %s.%s..." % (func.__module__, func.__name__)
-                    CProfileMethodRunner.profiler.enable()
+                if func_module not in CProfileMethodRunner.profiler_mapping and self.init_new_profiler:
+                    CProfileMethodRunner.profiler_mapping[func_module] = cProfile.Profile()
+                    profiler = CProfileMethodRunner.profiler_mapping[func_module]
+                    print "Starting CProfiler on %s.%s..." % (func_module, func_name)
+                    profiler.enable()
                 elif self.init_new_profiler:
-                    print "CProfile is already running!"
+                    print "CProfile is already running for %s!" % func_module
                 # execute the actual function
                 result = func(*args, **kwargs)
                 return result
             finally:
-                if self.stop_profiler:
+                if func_module in CProfileMethodRunner.profiler_mapping and self.stop_profiler:
+                    profiler = CProfileMethodRunner.profiler_mapping.pop(func_module)
                     if self.save_stats:
-                        CProfileMethodRunner.profiler.dump_stats(self.profiling_output)
+                        profiler.dump_stats(self.profiling_output)
                     else:
                         with open(self.profiling_output, 'w+') as stream:
-                            stats = pstats.Stats(CProfileMethodRunner.profiler, stream=stream)
+                            stats = pstats.Stats(profiler, stream=stream)
                             stats.strip_dirs()
                             stats.sort_stats(-1)
                             stats.print_stats()
+
                     # disable the profiler and clean for the next session
-                    if CProfileMethodRunner.profiler and self.stop_profiler:
-                        CProfileMethodRunner.profiler.disable()
-                        del CProfileMethodRunner.profiler
-                        CProfileMethodRunner.profiler = None
-                        print "Ending CProfiler on %s.%s..." % (func.__module__, func.__name__)
+                    profiler.disable()
+                    del profiler
+                    print "Ending CProfiler on %s.%s..." % (func_module, func_name)
 
         return wrapped_f
 
 
 class CProfileRunner(object):
     """
-    How to use:
+    Usage::
 
     from sgtk import CProfileRunner
 
